d60eefbd82e3c49615119d1d8d0e35741b1b3a6b
Halil Hodžić <habetech@gmail.com>
2025-08-30 21:44:21 +0200

    fix: bind logger + alias 'log'; wire Exception handler

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,46 +2,41 @@
 
 namespace App\Providers;
 
-use TrueFrame\Application;
 use TrueFrame\Support\ServiceProvider;
 use TrueFrame\Exceptions\Handler;
-use TrueFrame\Log\Logger;
+use TrueFrame\Log\Logger as TFLogger;
 use TrueFrame\Session\SessionManager;
+use TrueFrame\Config\Repository as ConfigRepo;
+use Psr\Log\LoggerInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register(): void
     {
-        // Bind the Exception Handler
+        // Logger (glavni binding)
+        $this->app->singleton(TFLogger::class, function ($app) {
+            /** @var ConfigRepo $config */
+            $config = $app->make(ConfigRepo::class);
+            return new TFLogger($config);
+        });
+
+        // PSR-3 alias i string aliasi (pokrij sve slučajeve)
+        $this->app->alias(TFLogger::class, LoggerInterface::class);
+        $this->app->alias(TFLogger::class, 'log');
+
+        // Exception handler koristi logger iz kontejnera
         $this->app->singleton(Handler::class, function ($app) {
-            return new Handler($app, $app->make(Logger::class));
+            return new Handler($app, $app->make(TFLogger::class));
         });
 
-        // Bind Logger
-        $this->app->singleton(Logger::class, function ($app) {
-            return new Logger($app->make(\TrueFrame\Config\Repository::class));
-        });
-
-        // Bind Session Manager
-        $this->app->singleton(SessionManager::class, function ($app) {
-            return new SessionManager();
-        });
+        // Session
+        $this->app->singleton(SessionManager::class, fn() => new SessionManager());
         $this->app->alias('session', SessionManager::class);
 
-        // Register the exception handler
+        // Registruj globalni handler
         $this->app->make(Handler::class)->register();
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot(): void
     {
         //
