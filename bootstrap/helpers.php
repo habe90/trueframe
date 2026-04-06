1d827aa4b95585ec8b40b1bce38b8b811d33b137
Halil Hodzic <habetech@gmail.com>
2026-04-07 00:49:49 +0200

    fix(bootstrap): add getallheaders() polyfill for CLI and CGI SAPIs
    - PHP's built-in getallheaders() only exists under Apache/FPM SAPIs
    - Add polyfill that parses $_SERVER['HTTP_*'] keys and reconstructs header names
    - Enables framework boot and request handling in CLI (artisan-style) and CGI contexts
    - Includes Content-Type and Content-Length which lack the HTTP_ prefix

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,4 +1,16 @@
 <?php
 
-// This file is empty because all core helpers are loaded from trueframe/framework/src/TrueFrame/Support/helpers.php
-// However, if you need application-specific helpers, you can add them here.
+// Polyfill getallheaders() for CLI and CGI SAPIs where it's not available
+if (!function_exists('getallheaders')) {
+    function getallheaders(): array
+    {
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (str_starts_with($name, 'HTTP_')) {
+                $key = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))));
+                $headers[$key] = $value;
+            }
+        }
+        return $headers;
+    }
+}
